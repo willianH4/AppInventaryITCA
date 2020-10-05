489b37aaa0472af9a4a2ac37a31d09adb1c0e68a
Adonis Bautista <adonisbautista39@gmail.com>
2020-10-05 15:52:09 -0600

    Eh Agregado La clase Acerca_dexd.java y tambien los layout que son activity_login.xml , activity_login_ejemplo.xml , activity_main.xml , activity_main_producto.xml , activity_main_usuario.xml .

---
+++ app/src/main/java/com/willianhdz/appinventaryitca/Acerca_dexd.java
@@ -0,0 +1,16 @@
+package com.willianhdz.appinventaryitca;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Acerca_dexd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acerca_de);
+    }
+}
